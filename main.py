37600be11bcd6336aac7c35e0ff0a323cd0795b8
leonardo <leonardo.lastra@gmail.com>
2020-12-23 09:38:43 -0300

    Extrae el ViewState del primer request

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ import requests
 req = readRequest()
 
 # Solicitud inicial donde se captura la cookie
-cookie = requests.request("GET", req[0]["url"]).headers["Set-Cookie"]
 
-for r in req:
-# incorpora la cookie en el header de nueva request
-    h = updateCookie(r,cookie)
-#Serie de requests.
-    requests.request("GET", r["url"], headers=h)
+response = requests.request("GET", req[0]["url"])
+cookie = response.headers["Set-Cookie"]
+soup = BeautifulSoup(response.text,"html5lib")
+ViewState= soup.select("#javax.faces.ViewState")[0]["value"]
 
-
-click = list(r for r in req if r["method"]=="POST")[0]
-h = updateCookie(click,cookie)
-res = requests.request("POST", click["url"], headers=h, data=click["postData"])
-print(res.text)
-soup = BeautifulSoup(res.text,"lxml")
-print(soup.select(".dor_organismos_panel"))
-print(res.headers)
+# for r in req:
+# # incorpora la cookie en el header de nueva request
+#     h = updateCookie(r,cookie)
+# #Serie de requests.
+#     requests.request("GET", r["url"], headers=h)
 
 
+# click = list(r for r in req if r["method"]=="POST")[0]
+# h = updateCookie(click,cookie)
+# res = requests.request("POST", click["url"], headers=h, data=click["postData"])
+# print(res.text)
+# soup = BeautifulSoup(res.text,"lxml")
+# print(soup.select(".dor_organismos_panel"))
+# print(res.headers)
