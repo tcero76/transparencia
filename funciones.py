cffb929139fe6a52359eb485169190409d4fbc70
leonardo <leonardo.lastra@gmail.com>
2021-01-11 19:10:51 -0300

    nueva página web

--- funciones.py
+++ funciones.py
@@ -2,35 +2,31 @@ import json
 from urllib.parse import quote_plus
 import requests
 from bs4 import BeautifulSoup
-import urllib
 
 def readRequest():
   with open('./transparencia.json') as f:
     data = json.load(f)
   # Filtrar serie
-  serie = data["log"]["entries"]
-  req = []
-  for request in serie:
-    if "https://www.portaltransparencia.cl/PortalPdT/web/guest/directorio-de-organismos-regulados" in request["request"]["url"]:
-      req.append(request["request"])
-  return req
+  return data["log"]["entries"]
 
 def getCookieAndViewState():
-    req = readRequest()
-    response = requests.request("GET", req[0]["url"])
-    cookie = response.headers["Set-Cookie"]
-    soup = BeautifulSoup(response.text, "html5lib")
+    entries = readRequest()
+    url = "https://www.portaltransparencia.cl/PortalPdT/buscador-directorio-de-organismos"
+    request = list(entrie["request"] for entrie in entries if url in entrie["request"]["url"])
+    res = requests.request("GET", request[0]["url"])
+    cookie = res.headers["Set-Cookie"]
+    soup = BeautifulSoup(res.text, "html5lib")
     ViewState = soup.select("input#javax.faces.ViewState")[0]["value"]
     return ViewState, cookie, soup
 
-def clickPanel1(cookie, ViewState, item):
-    req = readRequest()
-    click = list(r for r in req if r["method"]=="POST")[3]
-    h = updateCookie(click,cookie)
-    form = updateHeader(click,ViewState,item["name"])
-    res = requests.request("POST", click["url"], headers=h, data=form)
+def clickPanel1(cookie, ViewState, item=None):
+    entries = readRequest()
+    request = list(entrie["request"] for entrie in entries if entrie["request"]["method"]=="POST")[0]
+    h = updateCookie(request,cookie)
+    form = updateViewState(request,ViewState)
+    res = requests.request("POST", request["url"], headers=h, data=form)
     soup = BeautifulSoup(res.text,"html5lib")
-    return soup, click, h
+    return soup, res
 
 #Crear header nuevo
 def updateCookie(req, cookie):
@@ -45,6 +41,16 @@ def updateCookie(req, cookie):
       if not(cookieFlag):
           h["Cookie"] = cookie
   return h
+
+def updateViewState(click,ViewState):
+    ViewState = quote_plus(ViewState)
+    form = ""
+    for i in click["postData"]["text"].split("&"):
+        if "javax.faces.ViewState" == i.split("=")[0]:
+            form += "javax.faces.ViewState=" + ViewState + "&"
+        else:
+            form += i.split("=")[0] + "=" + i.split("=")[1] + "&"
+    return form
 
 # actualiza el parámetro ViewState en el body de la request
 def updateHeader(click,ViewState,source):
--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-from funciones import updateHeader, getCookieAndViewState, clickPanel1
-from bs4 import BeautifulSoup
-import requests
+from funciones import getCookieAndViewState, clickPanel1
+
 
 # Solicitud inicial donde se captura la cookie
 ViewState, cookie, soup = getCookieAndViewState()
-
-panel1= []
-for i in soup.select("a.dor_org_no_senialado"):
-    panel1.append({"nombre": i.text, "name" : i["name"] })
-
-ress = []
-for item in panel1:
-    soup2, click, h = clickPanel1(cookie, ViewState, item)
-    panel2 = []
-    for i in soup2.select("a.dor_organismos_selecc.Class_id_link_org_link"):
-        panel2.append({
-            "nombre":i.text,
-            "name": i["name"]
-        })
-    print(panel2)
-
-    for panel in panel2:
-        url = (soup.select("form#A3684:form")[0]["action"])
-        form = updateHeader(click,ViewState,panel["name"])
-        res = requests.request("POST", url , headers=h, data=form)
-        soup3 = BeautifulSoup(res.text,"html5lib")
-        ress.append(res)
-        print(soup3.select("a.btn.btn-default.btn-block.active.estilo_info_ta")[0]["href"])
-        print(soup3.select("a.btn.btn-default.btn-block.active.estilo_info_ta")[0].text)
+soup2, res  =clickPanel1(cookie, ViewState)
+for item in soup2.select("a.org-dir"):
+    print(item["href"])
 
